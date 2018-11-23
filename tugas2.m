e6efd2d396b476a7c6cc95c8b1ca31241f032bf5
muadzfn <43716592+muadzfn@users.noreply.github.com>
2018-11-23 13:32:57 +0700

    Add files via upload

---
+++ tugas2.m
@@ -0,0 +1,11 @@
+%tugas2
+A = input('masukkan A: ');
+B = input('masukkan B: ');
+if A>B
+    hasil ='A lebih besar dari B';
+elseif A<B
+    hasil ='A lebih kecil dari B';
+else
+    hasil = 'A sama dengan B';
+end
+disp(hasil);
