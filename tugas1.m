380e02e4d317badae8cd25e494317e931af13257
muadzfn <43716592+muadzfn@users.noreply.github.com>
2018-11-23 13:29:48 +0700

    Add files via upload

---
+++ tugas1.m
@@ -0,0 +1,13 @@
+%tugas1
+angka = input('masukkan angka: ');
+A = [1 4; 2 3]
+B = [4 5; 2 3]
+jumlah = A+B;
+kurang = A-B;
+Bagi = A/B;
+PangkatA = A^2;
+PangkatB = B^2;
+KaliMatriks = A*B;
+KaliElemen = A.*B;
+KaliSkalarA = A*angka;
+KaliSkalarB = B*angka;
